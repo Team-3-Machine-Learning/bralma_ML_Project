1405db1f7b03c7b708b104d46c2668a9a9aae9a3
MauroL04 <114081458+MauroL04@users.noreply.github.com>
2025-11-20 16:00:07 +0100

    Ml remake file structure (#22)
    * Restructured whole project.
    
    * Added combined files
    
    * Removed windows only install to prevent error on macos
    
    * Updated notebooks, and added training nodebook. Updated gitignore so that the trained model is not pushed (too big)
    
    * Updated training and created api base.
    
    * Fixed api baseaddress
    
    ---------
    
    Co-authored-by: ML1324 <mauro.leonardo.04@outlook.com>
    Co-authored-by: Brent <r0888481@student.thomasmore.be>

---
+++ Bralma_backend/Program.cs
@@ -0,0 +1,49 @@
+using Bralma_backend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.IncludeFields = true;
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/ukhousing", (UkHousingRequest request) =>
+{
+    var prediction = $"Dit is een prediction test :)";
+
+    // Call aws for response
+
+    UkHousingResponse response = new(prediction, PropertyType.D);
+
+    return Results.Ok(response);
+})
+.WithName("GetUkHousingPrediction");
+
+
+app.MapPost("/elecdemand", (ElecDemandRequest request) =>
+{
+    var prediction = $"Dit is een prediction test :)";
+
+    // Call aws for response
+    double england_wales_demand = 128;
+
+    ElecDemandResponse response = new(prediction, england_wales_demand);
+
+    return Results.Ok(response);
+})
+.WithName("GetElecDemandPrediction");
+
+app.Run();
