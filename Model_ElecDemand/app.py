4462c62f9a3947d010bf11d97e1b518c38677e0d
ML1324 <mauro.leonardo.04@outlook.com>
2025-11-22 16:24:21 +0100

    Fixed for regression

--- Model_ElecDemand/app.py
+++ Model_ElecDemand/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pycaret.classification import load_model, predict_model
+from pycaret.regression import load_model, predict_model
 import pandas as pd
 
 app = FastAPI()
@@ -9,9 +9,6 @@ model = load_model("./england_wales_demand_predictor")
 @app.post("/predict")
 def predict(data: dict):
     df = pd.DataFrame([data])
-
-    if hasattr(model, "silent"):
-        delattr(model, "silent")
 
     pred_df = predict_model(model, df)
     predicted_value = pred_df.loc[0, "Label"] if "Label" in pred_df.columns else pred_df.iloc[0, 0]
