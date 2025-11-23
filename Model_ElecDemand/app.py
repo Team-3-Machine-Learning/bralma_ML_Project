a37e723cd460ef1785b5fe5a317fb627923ee2db
MauroL04 <114081458+MauroL04@users.noreply.github.com>
2025-11-23 21:36:41 +0100

    Ml host ai (#30)
    * Added files
    
    * Added runtime
    
    * Fixed requirements
    
    * Switched pkl and names
    
    * Fixed name and pkl
    
    * Added pycaret
    
    * Fixed requirements
    
    * Requirements update
    
    * Added pkl
    
    * Fixed requirements
    
    * Added pkl for UKHousing and code for the hosting.
    
    * Fixed bugs and add root.
    
    * Fixed naming error.
    
    * Fixed error in app.py
    
    * Fixed for regression
    
    * CHanged app.py root print
    
    * Fixed requirements
    
    * Fixed 'silent' issue
    
    * fix
    
    * Fixed versions
    
    * Fixed version
    
    * Fixed version
    
    * Fixed versions
    
    * fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * fix
    
    * Fix
    
    * fix
    
    * Fix
    
    * fix
    
    * Fix
    
    * updated requirements
    
    * New model
    
    * New notebook
    
    * New model and app.py
    
    * New model and updated backend
    
    * New model
    
    * Changed app.py
    
    * Fixed error
    
    * Updated readme
    
    * Added dockerfile
    
    ---------
    
    Co-authored-by: ML1324 <mauro.leonardo.04@outlook.com>

--- Model_ElecDemand/app.py
+++ Model_ElecDemand/app.py
@@ -9,11 +9,10 @@ model = load_model("./england_wales_demand_model")
 @app.post("/predict")
 def predict(data: dict):
     df = pd.DataFrame([data])
-
-    pred_df = predict_model(model, df)
-    predicted_value = pred_df.loc[0, "Label"] if "Label" in pred_df.columns else pred_df.iloc[0, 0]
-
-    return {"EnglandWalesDemand": predicted_value}
+    pred = predict_model(model, df)
+    return {
+        "label": pred.loc[0, "prediction_label"],
+    }
 
 @app.get("/")
 def root():
