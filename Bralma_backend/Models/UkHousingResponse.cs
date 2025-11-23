98697ebafcf0ddf2d64ac1c3f36d3ff1f3a800ba
ML1324 <mauro.leonardo.04@outlook.com>
2025-11-23 19:58:30 +0100

    New model and updated backend

--- Bralma_backend/Models/UkHousingResponse.cs
+++ Bralma_backend/Models/UkHousingResponse.cs
@@ -2,22 +2,11 @@ namespace Bralma_backend.Models
 {
     public record class UkHousingResponse
     {
-        public string ResponseMessage = "Succes"!;
-        public PropertyType PropertyType;
+        public string PropertyType;
 
-        public UkHousingResponse(string responseMessage, PropertyType propertyType)
+        public UkHousingResponse(string propertyType)
         {
-            ResponseMessage = responseMessage;
             PropertyType = propertyType;
         }
-    }
-
-    public enum PropertyType
-    {
-        F,
-        D,
-        S,
-        T,
-        O,
     }
 }
